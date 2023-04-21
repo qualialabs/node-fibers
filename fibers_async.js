f1a3fe5e096bb851d20f17ffa3abae31d65777db
Zack Newsham <zack.newsham@gmail.com>
2023-04-21 07:49:39 -0400

    cleanup of fibers_async, enable node 16 in more envs,

--- fibers_async.js
+++ fibers_async.js
@@ -1,26 +1,12 @@
 const { AsyncResource } = require('async_hooks');
-const aw = process.binding('async_wrap');
 const _Fiber = require('./fibers_sync.js');
 
-const weakMap = new WeakMap();
+const asyncResourceWeakMap = new WeakMap();
 function Fiber(fn, ...args) {
-  if (!(this instanceof Fiber)) {
-    return new Fiber(fn, ...args);
-  }
-  const actualFn = fn; /*(...fnArgs) => {
-    return globalAr.runInAsyncScope(() => {
-      const ar = new AsyncResource('Fiber');
-      return ar.runInAsyncScope(() => fn(...fnArgs));
-    });
-  };*/
-  const _private = {
-    _fiber: _Fiber(actualFn, ...args),
-    _ar: new AsyncResource('Fiber'),
-  };
-
-  weakMap.set(this, _private);
-  _private._fiber._f = this;
-  return this;
+  const _fiber = _Fiber(fn, ...args);
+  const ar = new AsyncResource('Fiber');
+  asyncResourceWeakMap.set(_fiber, ar);
+  return _fiber;
 };
 
 Fiber.__proto__ = _Fiber;
@@ -28,9 +14,14 @@ Fiber.prototype = _Fiber.prototype;
 
 Object.defineProperty(Fiber, 'current', {
   get() {
-    return _Fiber.current && _Fiber.current._f;
+    return _Fiber.current;
   }
 })
+
+
+_Fiber.prototype.runInAsyncScope = function runInAsyncScope(fn) {
+  return asyncResourceWeakMap.get(this).runInAsyncScope(fn);
+};
 
 _Fiber[Symbol.hasInstance] = function(obj) {
   // hacky
@@ -38,30 +29,3 @@ _Fiber[Symbol.hasInstance] = function(obj) {
 };
 
 module.exports = Fiber;
-Fiber.prototype = {
-  __proto__: Fiber,
-
-  get _fiber() {
-    return weakMap.get(this)._fiber;
-  },
-
-  get started() {
-    return this._fiber.started;
-  },
-
-  runInAsyncScope(fn) {
-    return weakMap.get(this)._ar.runInAsyncScope(fn);
-  },
-
-  run(...args) {
-    return this._fiber.run(...args);
-  },
-
-  throwInto(...args) {
-    return this._fiber.throwInto(...args);
-  },
-
-  reset(...args) {
-    return this._fiber.reset(...args);
-  }
-}
